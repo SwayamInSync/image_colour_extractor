f6bd882fff895cfd241540730d1fc84277ff4067
Swayam <74960567+practice404@users.noreply.github.com>
2021-06-05 10:09:32 +0530

    update main.py

--- main.py
+++ main.py
@@ -16,24 +16,23 @@ def home():
             flash("Please select image first")
             return render_template('display1.html')
         img = Image.open(file)
+        r, c = img.size
+
+        if r * c > 1000 * 1000:
+            basewidth = 640
+            wpercent = (basewidth / float(img.size[0]))
+            hsize = int((float(img.size[1]) * float(wpercent)))
+            img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+
         img_array = np.array(img)
         rows, columns, colours = img_array.shape
         vector_len = rows * columns * colours
         pixels = vector_len / 3
 
-        if pixels > 1000*1000:
-            basewidth = 640
-            wpercent = (basewidth / float(img.size[0]))
-            hsize = int((float(img.size[1]) * float(wpercent)))
-            img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-            img_array = np.array(img)
-            rows, columns, colours = img_array.shape
-            vector_len = rows * columns * colours
-            pixels = vector_len / 3
-
+        print(img_array.shape)
         colors = []
         for row in range(rows):
-            for column in range(columns//3):
+            for column in range(columns // 3):
                 r = img_array[row, column, :][0]
                 g = img_array[row, column, :][1]
                 b = img_array[row, column, :][2]
@@ -57,8 +56,8 @@ def home():
             num = 5
 
         top_10_colors = [c for c in color_freq.keys()][::num]
-#         top_10_colors = top_10_colors[
-#                         :: (len(top_10_colors) // 10) if (len(top_10_colors) // 10) else (len(top_10_colors))]
+        #         top_10_colors = top_10_colors[
+        #                         :: (len(top_10_colors) // 10) if (len(top_10_colors) // 10) else (len(top_10_colors))]
         return render_template('display1.html', colors=top_10_colors, total_colors=len(color_freq))
     return render_template('display1.html')
 
